bc416c3b1001d08d94b738a7cba094e19bf84af9
jamieAmacpherson <macpherson.js@gmail.com>
2016-07-13 08:56:54 +0100

    edit pipe

--- umbrella_pipeline.sh
+++ umbrella_pipeline.sh
@@ -8,32 +8,32 @@ topol_g4="../../WTmetmd/topol_g455.tpr"
 index="../../WTmetmd/umbrella.ndx"
 
 # Generate configurations #
-#mkdir structures
+mkdir structures
 
-#$trjconv -f $met -s $topol -o structures/conf.gro -sep -pbc mol <<EOF
-#0
-#EOF
+$trjconv -f $met -s $topol -o structures/conf.gro -sep -pbc mol <<EOF
+0
+EOF
 
 # Calculate distances #
 cd structures
 
-#last_struct="$(ls *.gro -Art | tail -n 1 | sed 's/[^0-9]*//g')"
+last_struct="$(ls *.gro -Art | tail -n 1 | sed 's/[^0-9]*//g')"
 # generate a file with the pulling and stationary groups (may have to change numbers here)
-#printf "15\n14\n" > groups.txt
+printf "15\n14\n" > groups.txt
 
-#cp ../distances.pl .
-#cp ../setupUmbrella.py .
-#echo $last_struct $topol_g4 $index
+cp ../distances.pl .
+cp ../setupUmbrella.py .
+echo $last_struct $topol_g4 $index
 
-#perl distances.pl $last_struct $topol_g4 $index
+perl distances.pl $last_struct $topol_g4 $index
 
 # identify structures for umbrella sampling #
-#cp ../run-umbrella.sh .
-#python setupUmbrella.py summary_distances.dat 0.05 run-umbrella.sh > caught-output.txt
+cp ../run-umbrella.sh .
+python setupUmbrella.py summary_distances.dat 0.05 run-umbrella.sh > caught-output.txt
 
-#awk '{print $1}' caught-output.txt > list-selected-structures.dat
-#sed -i -e 1,3d list-selected-structures.dat
-#awk '{print $1}' summary_distances.dat > all-structures.dat
+awk '{print $1}' caught-output.txt > list-selected-structures.dat
+sed -i -e 1,3d list-selected-structures.dat
+awk '{print $1}' summary_distances.dat > all-structures.dat
 
 # move structures for US into new directory
 mkdir directory-umbrella-sampling
